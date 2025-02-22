925de3f07765c58b5389eea3d3b2cf3acf7303e8
ht1423 <tharshita0215@gmail.com>
2025-02-23 00:12:35 +0530

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import connectDB from './db.js'
+dotenv.config()
+
+connectDB()
+
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`))
