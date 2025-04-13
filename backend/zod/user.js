d387cfe208e790fb8a0cbfa9280e57e9809e6026
ht1423 <tharshita0215@gmail.com>
2025-04-13 18:44:16 +0530

    zod updated

--- backend/zod/user.js
+++ backend/zod/user.js
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const zodSignin = z.object({
     email: z.string().email("Invalid email format"),
-    password: z.string().min(8, "Password cannot be empty").max(128, "Password can be at most 128 characters long")
+    password: z.string().min(8, "Password cannot be empty or less than 8 characters").max(128, "Password can be at most 128 characters long")
 }).strict()
 
 const zodSignup = zodSignin.extend({
