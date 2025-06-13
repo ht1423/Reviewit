4f15e629a1ce5a96d431ebdda3cb47c813aef817
ht1423 <tharshita0215@example.com>
2025-06-13 11:45:20 +0530

    dashboard updated

--- frontend/src/components/workspace/Inbox.jsx
+++ frontend/src/components/workspace/Inbox.jsx
@@ -3,7 +3,7 @@ import InboxElements from './InboxElements';
 
 function Inbox() {
   return (
-    <div className="text-center mt-32">
+    <div className="text-center mt-20 sm:mt-32">
       <div className="text-[26px] font-semibold text-zinc-300 mb-12">Inbox</div>
       <div className="flex justify-center items-center flex-wrap gap-10">
         {['All', 'Text', 'Image', 'Video', 'Liked'].map((type) => (
