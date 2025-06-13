18c86acfbe682f79bc0a043e455d05fe0911db38
ht1423 <tharshita0215@example.com>
2025-06-13 09:01:12 +0530

    just checking

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB()
 
 app.use(express.json())
 app.use(cors({
-    origin: "https://my-reviewit.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(cookieParser())
